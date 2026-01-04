bd0e29a0144bf2f32075d046ed7c87b5886e7369
tomaisthorpe <tomaisthorpe@gmail.com>
2026-01-04 15:50:12 +0000

    fix: temporarily disable migrate on startup to fix prisma 7 issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "Running database migrations..."
-npx prisma migrate deploy
+# TEMPORARILY DISABLED: prisma migrate deploy is broken in Prisma 7.2.0 with Docker
+# See: https://github.com/prisma/prisma/issues/28983
+# TODO: Re-enable once Prisma fixes the bug or upgrade to a patched version
+# echo "Running database migrations..."
+# npx prisma migrate deploy
 
 # Create admin user if credentials are provided
 if [ -n "$ADMIN_EMAIL" ] && [ -n "$ADMIN_PASSWORD" ]; then
