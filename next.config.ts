08a22069001a97f944f1c6e257c04f28ab0abb86
tomaisthorpe <tomaisthorpe@gmail.com>
2026-01-08 18:51:06 +0000

    feat: add hsts header

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,19 @@ const nextConfig: NextConfig = {
       "./node_modules/jiti/**/*",
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const bundleAnalyzer = withBundleAnalyzer({
