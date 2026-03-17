892c482c4cbcc6fcdd792f2f0e88083f19e2b68b
tomaisthorpe <tomaisthorpe@gmail.com>
2026-03-17 19:25:36 +0000

    fix: remove middleware from prisma instrumentation

--- instrumentation.ts
+++ instrumentation.ts
@@ -4,6 +4,6 @@ import { PrismaInstrumentation } from "@prisma/instrumentation";
 export function register() {
   registerOTel({
     serviceName: "unwhelm",
-    instrumentations: [new PrismaInstrumentation({ middleware: true })],
+    instrumentations: [new PrismaInstrumentation()],
   });
 }
